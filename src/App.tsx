439a54b1f97081aecf3900d0f3fc0614368dd0d0
EgorZapatylak <zapatylak96a@gmail.com>
2023-08-21 20:40:23 +0300

    Added new components Todolist

--- src/App.tsx
+++ src/App.tsx
@@ -4,25 +4,31 @@ import './App.css';
 function App() {
     return (
         <div className="App">
-            <div>
-                <h3>What to learn</h3>
-                <div>
-                    <input/>
-                    <button>+</button>
-                </div>
-                <ul>
-                    <li><input type="checkbox" checked={true}/> <span>HTML&CSS</span></li>
-                    <li><input type="checkbox" checked={true}/> <span>JS</span></li>
-                    <li><input type="checkbox" checked={false}/> <span>React</span></li>
-                </ul>
-                <div>
-                    <button>All</button>
-                    <button>Active</button>
-                    <button>Completed</button>
-                </div>
-            </div>
+            <Todolist/>
         </div>
     );
+}
+
+function Todolist() {
+    return (
+        <div>
+            <h3>What to learn</h3>
+            <div>
+                <input/>
+                <button>+</button>
+            </div>
+            <ul>
+                <li><input type="checkbox" checked={true}/> <span>HTML&CSS</span></li>
+                <li><input type="checkbox" checked={true}/> <span>JS</span></li>
+                <li><input type="checkbox" checked={false}/> <span>React</span></li>
+            </ul>
+            <div>
+                <button>All</button>
+                <button>Active</button>
+                <button>Completed</button>
+            </div>
+        </div>
+    )
 }
 
 export default App;
