497c532ef0815b30da1a444b5113cf5197e76e0b
EgorZapatylak <zapatylak96a@gmail.com>
2023-08-21 21:01:33 +0300

    Typing props tasktype in Todolist.tsx

--- src/Todolist.tsx
+++ src/Todolist.tsx
@@ -1,7 +1,14 @@
 import React from "react";
 
+type TaskType = {
+    id: number
+    title: string
+    isDone: boolean
+}
+
 type PropsTodolistType = {
     title:string
+    tasks:Array<TaskType>
 }
 
 export function Todolist(props: PropsTodolistType) {
@@ -13,9 +20,9 @@ export function Todolist(props: PropsTodolistType) {
                 <button>+</button>
             </div>
             <ul>
-                <li><input type="checkbox" checked={true}/> <span>HTML&CSS</span></li>
-                <li><input type="checkbox" checked={true}/> <span>JS</span></li>
-                <li><input type="checkbox" checked={false}/> <span>React</span></li>
+                <li><input type="checkbox" checked={props.tasks[0].isDone}/> <span>{props.tasks[0].title}</span></li>
+                <li><input type="checkbox" checked={props.tasks[1].isDone}/> <span>{props.tasks[1].title}</span></li>
+                <li><input type="checkbox" checked={props.tasks[2].isDone}/> <span>{props.tasks[2].title}</span></li>
             </ul>
             <div>
                 <button>All</button>
