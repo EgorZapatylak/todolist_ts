35017d30b7daca971ab3851d88718c53fa66ee25
EgorZapatylak <zapatylak96a@gmail.com>
2023-08-21 20:45:10 +0300

    Moved the component to a separate component

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import './App.css';
+import {Todolist} from "./Todolist";
 
 function App() {
     return (
@@ -9,28 +10,6 @@ function App() {
             <Todolist/>
         </div>
     );
-}
-
-function Todolist() {
-    return (
-        <div>
-            <h3>What to learn</h3>
-            <div>
-                <input/>
-                <button>+</button>
-            </div>
-            <ul>
-                <li><input type="checkbox" checked={true}/> <span>HTML&CSS</span></li>
-                <li><input type="checkbox" checked={true}/> <span>JS</span></li>
-                <li><input type="checkbox" checked={false}/> <span>React</span></li>
-            </ul>
-            <div>
-                <button>All</button>
-                <button>Active</button>
-                <button>Completed</button>
-            </div>
-        </div>
-    )
 }
 
 export default App;
