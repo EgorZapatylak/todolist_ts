8cf3d518aae2d97d74ba38775fa350409460d424
EgorZapatylak <zapatylak96a@gmail.com>
2023-08-29 23:10:55 +0300

    Used hook useState in App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,26 @@
-import React from 'react';
+import React, {useState} from 'react';
 import './App.css';
 import {Todolist} from "./Todolist";
 
 function App() {
 
-    let tasks1 = [
+    let [tasks,setTasks] = useState([
         {id: 1, title: "HTML&CSS", isDone: true},
         {id: 2, title: "JS", isDone: true},
         {id: 3, title: "ReactJS", isDone: false},
         {id: 4, title: "Reset API", isDone: false},
         {id: 5, title: "GraphQL", isDone: false},
-    ]
+    ])
 
     function removeTask(id: number) {
-        tasks1 = tasks1.filter(task => task.id != id)
-        console.log(tasks1)
+        let filteredTasks = tasks.filter(task => task.id != id)
+        setTasks(filteredTasks)
     }
 
 
     return (
         <div className="App">
-            <Todolist title="What to learn" tasks={tasks1} removeTask={removeTask}/>
+            <Todolist title="What to learn" tasks={tasks} removeTask={removeTask}/>
         </div>
     );
 }
