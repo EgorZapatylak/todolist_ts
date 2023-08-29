a126e624548668580285757fba1e86d582917427
EgorZapatylak <zapatylak96a@gmail.com>
2023-08-29 22:58:49 +0300

    Added function delete task

--- src/App.tsx
+++ src/App.tsx
@@ -4,7 +4,7 @@ import {Todolist} from "./Todolist";
 
 function App() {
 
-    const tasks1 = [
+    let tasks1 = [
         {id: 1, title: "HTML&CSS", isDone: true},
         {id: 2, title: "JS", isDone: true},
         {id: 3, title: "ReactJS", isDone: false},
@@ -12,10 +12,15 @@ function App() {
         {id: 5, title: "GraphQL", isDone: false},
     ]
 
+    function removeTask(id: number) {
+        tasks1 = tasks1.filter(task => task.id != id)
+        console.log(tasks1)
+    }
+
 
     return (
         <div className="App">
-            <Todolist title="What to learn" tasks={tasks1}/>
+            <Todolist title="What to learn" tasks={tasks1} removeTask={removeTask}/>
         </div>
     );
 }
