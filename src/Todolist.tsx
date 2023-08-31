4d4261b5cb3cc0ba8db8863bb7bb170721940f76
EgorZapatylak <zapatylak96a@gmail.com>
2023-08-31 23:24:29 +0300

    Added callback function addTask in Todolist.tsx

--- src/Todolist.tsx
+++ src/Todolist.tsx
@@ -18,12 +18,18 @@ type PropsTodolistType = {
 
 export function Todolist(props: PropsTodolistType) {
     let [title,setTitle]= useState('')
+
+    const addTask = () => {
+        props.addTask(title)
+        setTitle('')
+    }
+
     return (
         <div>
             <h3>{props.title}</h3>
             <div>
                 <input value={title} onChange={(event)=>{setTitle(event.currentTarget.value)}}/>
-                <button onClick={()=>(props.addTask(title))}>+</button>
+                <button onClick={addTask}>+</button>
             </div>
             <ul>
                 {props.tasks.map((task) => {
